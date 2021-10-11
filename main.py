0d36d37bff6884786b65500e256bb681ea8d293d
Tikhonov-Vladimir <63228981+Tikhonov-Vladimir@users.noreply.github.com>
2021-10-11 22:02:34 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 import json
+
 file = open("pokemon_full.json")
 str_json = file.read()
-L = len(str_json)
-print("Общее количество символов в файле равно", L)
+len_str = len(str_json)
+print("Общее количество символов в файле равно", len_str)
 file.close()
 
 file = open("pokemon_full.json")
-str_correct = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" # Создаем строку символов, которые мы будем считать.
-lines = file.readlines()
-f = ""
 counter = 0
-for line in lines:
-    f += line
-for i in f:
-    if i in str_correct: # Если символ содержится в str_correct, то считаем его.
+for i in str_json:
+    if i.isalnum() is True:
         counter += 1
 print("Общее количесто символов без пробелов и знаков препинания равно", counter)
 file.close()
 
-data = json.loads(str_json) # Чтобы преобразовать строки json в объекты Python, используем функцию json.loads()
+data = json.loads(str_json)
 max_desc = 0
 poke_name = ""
 for profile in data:
