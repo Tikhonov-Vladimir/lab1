3c05f8559c44e4883d4136af8c6da45c33312c0d
Tikhonov-Vladimir <63228981+Tikhonov-Vladimir@users.noreply.github.com>
2021-10-13 17:38:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import time
 import json
+
+start = time.time()
 
 file = open("pokemon_full.json")
 str_pokemon = file.read()
@@ -14,19 +17,20 @@ print("Общее количесто символов без пробелов и
 
 objects = json.loads(str_pokemon)
 max_desc = 0
-poke_name = ""
-for profile in objects:
-    desc = profile["description"]
-    if len(desc) > max_desc:
-        max_desc = len(desc)
-        poke_name = profile["name"]
-print("У покемона", poke_name, "наиболее длинное описание")
-
 max_words = 0
+poke_name = ""
 abilities = ""
 for profile in objects:
+    desc = profile["description"]
     for skills in profile["abilities"]:
         if len(skills.split()) > max_words:
             max_words = len(skills.split())
             abilities = skills
+    if len(desc) > max_desc:
+        max_desc = len(desc)
+        poke_name = profile["name"]
+print("У покемона", poke_name, "наиболее длинное описание")
 print("Умение", abilities, "содержит больше всего слов")
+
+end = time.time()
+print(f'Время выполнения {end-start}')
